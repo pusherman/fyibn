f82611e7e2a5a763642d31c4ab0a6dd7d0f0f7e7
Corey Wilson <phpuser@gmail.com>
2014-04-23 22:32:55 -0400

    removed unused whitespace

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -1,33 +1,33 @@
 <?php
 
-class PostController extends BaseController 
+class PostController extends BaseController
 {
 
     /**
      * Show all the posts
      */
-	public function showIndex()
-	{
-	    $posts = Post::latest()->paginate(25);
-        
-		return View::make('post.index', [
-		    'posts' => $posts
-		]);
-	}
+    public function showIndex()
+    {
+        $posts = Post::latest()->paginate(25);
+
+        return View::make('post.index', [
+            'posts' => $posts
+        ]);
+    }
 
     /**
      * Show the create post form
-     */ 
-	public function showCreate()
-	{
-    	return View::make('post.create');
-	}
+     */
+    public function showCreate()
+    {
+        return View::make('post.create');
+    }
 
-	/**
-	 * Create a new post
-	 */
-	public function create()
-	{
+    /**
+     * Create a new post
+     */
+    public function create()
+    {
         $post = Post::create(array(
             'title' => Input::get('title'),
             'url' => Input::get('url'),
