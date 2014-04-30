1436137f5fb411482d76fe2b0e9e5b62cf4a5282
Corey Wilson <phpuser@gmail.com>
2014-04-29 23:13:05 -0400

    auto link and add line breaks on comments

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -7,10 +7,12 @@ class Comment extends Eloquent
     );
 
     /**
-     * Update the updated_at column of the posts table 
+     * Update the updated_at column of the posts table
      * when a comment is added
      */
     protected $touches = array('post');
+
+
 
     /**
      * Comment to post relationship
@@ -26,5 +28,31 @@ class Comment extends Eloquent
     public function user()
     {
         return $this->belongsTo('User');
+    }
+
+    /**
+     * Format markdown on get
+     */
+    public function setBodyAttribute($text)
+    {
+        $text = strip_tags($text);
+
+        $text = str_replace("\r\n", "\n", $text);
+        $text = str_replace("\r", "\n", $text);
+
+        # replace tabs with spaces
+        $text = str_replace("\t", '    ', $text);
+
+        # remove surrounding line breaks
+        $text = trim($text, "\n");
+
+        $text = preg_replace("/\n/", " <br />", $text);
+
+        $text = preg_replace_callback('/(https?:\/\/([\w.]+\/?)\S*)/', function($matches) {
+            $link = $matches[0];
+            return "<a href=\"$link\">$link</a>";
+        }, $text);
+
+        $this->attributes['body'] = $text;
     }
 }
