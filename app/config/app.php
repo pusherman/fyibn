37c56a6f0f39bf45f80c91b6e94380c5c458cd42
Corey Wilson <phpuser@gmail.com>
2014-04-29 23:12:47 -0400

    removed old debug bar support

--- app/config/app.php
+++ app/config/app.php
@@ -105,9 +105,7 @@ return array(
 		'Illuminate\Translation\TranslationServiceProvider',
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
-		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Barryvdh\Debugbar\ServiceProvider',
-
+		'Illuminate\Workbench\WorkbenchServiceProvider'
 	),
 
 	/*
