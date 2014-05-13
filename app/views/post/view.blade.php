02f2f1f39355638c542f8d1496c256165b3bde2d
Corey Wilson <corey@ereatah.com>
2014-05-13 11:22:08 -0400

    attempting to move post metadata above post content

--- app/views/post/view.blade.php
+++ app/views/post/view.blade.php
@@ -4,40 +4,42 @@
 @section('content')
 
 <div class="page-header">
-    <div class="row" id="post-view">
-        <div class="col-md-1 dope-button-holder">
-            @include('post._dope_button')
-        </div>
+    <div class="container">
+        <div class="row" id="post-view">
+            <div class="col-md-1 dope-button-holder">
+                @include('post._dope_button')
+            </div>
 
-        <div class="col-md-11">
-            <section id="post-details">
-                <h2>
-                    @if ($post->url)
-                        {{{ $post->title }}}
-                    @else
-                        {{ link_to($post->url, $post->title, ['target' => '_blank']) }}
-                    @endif
-                    <br>
-                    <small>
-                        by {{ $post->user->username }}
-                        {{ $post->created_at->diffForHumans() }}
-                    </small>
-                </h2>
-            </section>
+            <div class="col-md-11">
+                <section id="post-details">
+                    <h2>
+                        @if ($post->url)
+                            {{{ $post->title }}}
+                        @else
+                            {{ link_to($post->url, $post->title, ['target' => '_blank']) }}
+                        @endif
+                        <br>
+                        <small>
+                            by {{ $post->user->username }}
+                            {{ $post->created_at->diffForHumans() }}
+                        </small>
+                    </h2>
+                </section>
+            </div>
         </div>
     </div>
 </div>
 
-<div class="comment-list">
-
-@foreach($post->comments as $comment)
-<div class="row comment" style="margin-bottom:15px">
-    <blockquote>
+<div class="comment-list container">
+    @foreach($post->comments as $comment)
+    <div class="row comment" style="margin-bottom:15px">
+        <div class="col-md-9">
+        <p>{{ $comment->user->username }} &middot; {{ $comment->created_at->diffForHumans() }}</p>
         <p>{{ $comment->body }}</p>
-        <footer><cite title="{{ $comment->created_at->diffForHumans() }}">{{ $comment->user->username }}</cite></footer>
-    </blockquote>
+        </div>
+    </div>
+    @endforeach
 </div>
-@endforeach
 
 <form role="form" method="post" action="{{ route('createComment', ['post_id' => $post->id]) }}">
     <div class="form-group">
