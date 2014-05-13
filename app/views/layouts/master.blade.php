4cc16039700725ba712a0b70b4f02aea66557e60
Paolo <mrtnpaolo@gmail.com>
2014-05-13 18:26:01 +0200

    no ‘content’ in the error’s @if, right?

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -43,13 +43,13 @@
         </div>
     @endif
 
-        @if (Session::has('error'))
-            <div class="alert alert-danger alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    @if (Session::has('error'))
+        <div class="alert alert-danger alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 
-                {{{ Session::get('error') }}}
-            </div>
-        @endif
+            {{{ Session::get('error') }}}
+        </div>
+    @endif
 
     @yield('content')
 
